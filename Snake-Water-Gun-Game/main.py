c3d2c6d4c7e3b8aad7f57e4cdde7d786b60bf17f
Haroon Khan <haroonreyz@gmail.com>
2025-01-29 12:49:48 +0500

    Added Snake Water Gun

--- Snake-Water-Gun-Game/main.py
+++ Snake-Water-Gun-Game/main.py
@@ -16,17 +16,23 @@ print(f" You chose {reversDict[you]} \n Computer chose {reversDict[computer]}")
 if(computer==you):
     print("Draw")
 else:
- if(computer==-1 and you==1):
+ if(computer==-1 and you==1): #* -2
     print("You Win")
- elif(computer == -1 and you==0):
+ elif(computer == -1 and you==0): #* -1
     print("You Lose")
- elif(computer==1 and you==-1):
+ elif(computer==1 and you==-1): #* 2
     print("You Lose")
- elif(computer==1 and you ==0):
+ elif(computer==1 and you ==0): #* 1
     print("You Win")
- elif(computer==0 and you==-1):
+ elif(computer==0 and you==-1): #* 1
     print("You Win")
- elif(computer==0 and you ==1):
+ elif(computer==0 and you ==1): #* -1
     print("You Lose")
  else:
     print("Something went wrong")
+
+#*=======================Short method==================================================================
+if((computer - you)==-1 or (computer - you)==2):
+   print("You Lose")
+else:
+   print("You Win")
