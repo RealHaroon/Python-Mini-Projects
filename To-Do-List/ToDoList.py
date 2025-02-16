0c8a208c88476891143718afe3049381f98dda5c
Haroon Khan <haroonreyz@gmail.com>
2025-02-16 13:32:27 +0500

    Updated To-Do-List (16-02-2025)

--- To-Do-List/ToDoList.py
+++ To-Do-List/ToDoList.py
@@ -1,50 +1,81 @@
-toDoLst=[]
-taskHistory={}
+#
+def addTask():
+    u_inp=input("Add Task : ")
+    try:
+     with open("To-Do-List\\task.txt","a")as f:
+        f.write(u_inp +"\n")
+        print("Task Added Succesfully")
+    except:
+       print("failed to add task")
 
-def addTask(task): # Func1
-    toDoLst.append(task)
+def veiwTask():
+   count=0
+   try:
+      with open("To-Do-List\\task.txt","r",encoding="utf-8") as f: #  UTF-8 encoding for emoji and check symbol 
+         lines=f.readlines()
+         for i in lines:
+            count+=1
+            print(count,".",i)
+   except:
+      print("Failed to veiw all task")
 
-def removeTask(task): # Func2
-    toDoLst.remove(task)
+def doneTask():
+  u_inp=input("Done Task: ")
+  try:
+   with open("To-Do-List\\task.txt", "r", encoding="utf-8") as f:  
+    lines = f.readlines()  # Read all lines first
+   with open("To-Do-List\\task.txt", "w", encoding="utf-8") as f:  
+    for line in lines:
+        if u_inp in line:
+            f.write(line.rstrip() + " ✔\n")  
+            print("Marked!")
+        else:
+            f.write(line)
+  except:
+     print("Failed to Mark Task Done!")
 
-def markDoneTask(task): # Func3
-    taskHistory.update({task:"Done"})
+def deleteTask():
+   u_inp=input("Remove Task: ")
+   try:
+    with open("To-Do-List\\task.txt", "r") as f:
+      lines=f.readlines()  # Read all tasks
+    with open("To-Do-List\\task.txt", "w") as f:
+      for line in lines:
+         if u_inp not in line:
+            f.write(line)  
+      print("Task Has been removed")
+   except:
+      print("Failed to remove task!")
 
-def showAllTask(): #Func4
-    count=0
-    for i in toDoLst:
-        count+=1
-        print(count,i)
-def showTaskHistory(): #FUnc 5
-     return taskHistory
+def clearTask():
+   try:
+      with open("To-Do-List\\task.txt", "w") as f:
+         f.truncate()
+         print("List Has been cleard!")
+   except:
+      print("Failed to clear task list!")
 
-while(True):
-    print("\t TO DO LIST")
-    print("Press 1 to Add Task.")
-    print("Press 2 to Remove Task.")
-    print("Press 3 to Show All Task.")
-    print("Press 4 to Marks Done Task.")
-    print("Press 5 to Show Task History.")
-    print("Press 6 to Exit.")
+print(" 📜 To-Do-List By Haroon ")
+while True:
+   print("1. Add Task")
+   print("2. Mark Task")
+   print("3. View Task")
+   print("4. Remove Task")
+   print("5. Clear To Do List")
+   print("6. Exit")
 
-    inp=input("Enter Your Choice : ")
-
-    if inp=="1":
-        addTsk=input("Add Task : ")
-        addTask(addTsk)
-    if inp=="2":
-        remTask=input("Remove Task : ").casefold()
-        removeTask(remTask)
-    if inp=="3":
-        print("All Tasks : ")
-        showAllTask()
-    if inp=="4":
-        mrkTask=input("Mark the done task: ")
-        markDoneTask(mrkTask)
-    if inp=="5":
-        print("Task History : ")
-        print(taskHistory)
-    if inp=="6":
-        print("Exited!")
-        break
-    
+   u_inp=input("Enter Your Choice (1 - 6) = ")
+   if u_inp == "1":
+      addTask()
+   elif u_inp == "2":
+      doneTask()
+   elif u_inp == "3":
+      veiwTask()
+   elif u_inp == "4":
+      deleteTask()
+   elif u_inp == "5":
+      clearTask()
+   elif u_inp == "6":
+      break
+   else:
+      print("Invalid Input!")
